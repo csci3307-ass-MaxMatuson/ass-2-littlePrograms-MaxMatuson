3fa8d903acc17939caa1af15a9bcde6019e1f79e
Max-IM97 <max.matuson@gmail.com>
2017-04-23 19:12:17 -0400

    Programs
    2.15
    2.16

--- src/com/mycompany/ass2/chapter2/Program_2_15.java
+++ src/com/mycompany/ass2/chapter2/Program_2_15.java
@@ -10,7 +10,8 @@ import java.util.Scanner;
  *
  */
 public class Program_2_15 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Scanner input = new Scanner(System.in);
 		
 		int num1, num2, sum, difference, product;
